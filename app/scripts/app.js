8e527b345490f4086402fc061a9168a31d44d4eb
lilybader <baderlily@gmail.com>
2015-07-15 22:58:32 -0700

    Added Firebase Back-End

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,18 @@ app.config(['$locationProvider', '$stateProvider', function($locationProvider, $
       });
 
       $stateProvider
-      .state('tasks', {
-        url: '/tasks',
+      .state('current', {
+        url: '/current',
         controller: 'taskController',
         templateUrl: "templates/currentTasks.html"
       });
 
+      $stateProvider
+      .state('expired', {
+        url: '/expired',
+        controller: 'taskExpiredController',
+        templateUrl: "templates/expiredTasks.html"
+      });
 
       $stateProvider
       .state('history', {
@@ -25,25 +31,55 @@ app.config(['$locationProvider', '$stateProvider', function($locationProvider, $
       });
   }]);
 
-app.controller('taskController', ['$scope', function ($scope) {
-    $scope.task = [
-        {'title': "Build a Task Manager", 'done':false}
-    ];
+app.controller('taskController', ['$scope', '$firebase', function ($scope, $firebase) {
+    var ref = new Firebase('https://bloc-it-off-timer.firebaseio.com/tasks');
+    var sync = $firebase(ref);
 
-    $scope.addTask = function() {
-        $scope.task.push({'title':$scope.newTask, 'done':false})
-        $scope.newTask = ''
-    }
-    $scope.clearCompleted = function() {
-        $scope.task = $scope.task.filter(function(item){
-            return !item.done
-        })
-    }
+    var tasks = sync.$asArray();
+    $scope.tasks = tasks;
+
+    $scope.addTask = function(task) {
+      $scope.tasks.$add({
+        task: task,
+        state: "active",
+        dateAdded: Firebase.ServerValue.TIMESTAMP
+      });
+      $scope.newTaskItem = "";
+    };
+
+    $scope.completeTask = function(taskId) {
+      var task = tasks.$getRecord(taskId);
+      task.state = "complete";
+      tasks.$save(task);
+    };
+
+    $scope.expireTask = function(taskId) {
+        var task = tasks.$getRecord(taskId);
+        var today = new Date();
+        var now = today.getTime();
+        var days = 24 * 60 * 60 * 1000 //hours * minutes * seconds * milliseconds
+        if (task.state == "active" && (now - task.dateAdded) >= (7 * days)) {
+            task.state = "expired";
+           tasks.$save(task);
+        }
+    };
+    
 }]);
 
 app.controller('taskHistoryController', ['$scope', function ($scope) {
-    
+    var ref = new Firebase("https://bloc-it-off-timer.firebaseio.com/tasks");
+    var sync = $firebase(ref);
 
+    var tasks = sync.$asArray();
+    $scope.tasks = tasks;
+}]);
+
+app.controller('taskExpiredController', ['$scope', function ($scope) {
+    var ref = new Firebase("https://bloc-it-off-timer.firebaseio.com/tasks");
+    var sync = $firebase(ref);
+
+    var tasks = sync.$asArray();
+    $scope.tasks = tasks;
 }]);
 
 
